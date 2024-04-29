715b10172d2ef3d746e72a01f532d31ed1357300
Kafy Dier <works.omen@gmail.com>
2024-04-29 16:56:44 +0530

    container adjustments

--- js/page.js
+++ js/page.js
@@ -7,12 +7,10 @@ pages.forEach(page => {
                 if (otherPage !== page && otherPage.classList.contains('active')) {
                     otherPage.classList.remove('active');
                     otherPage.querySelector('.page-title').classList.remove('active');
-                    otherPage.querySelector('.container-profile').classList.remove('active');
                 }
             });
             page.classList.add('active');
             page.querySelector('.page-title').classList.add('active');
-            page.querySelector('.container-profile').classList.add('active');
         }
     });
 });
