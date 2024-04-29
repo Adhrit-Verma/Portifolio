82d405d4c18b1728178fd7233df9c3a2c226a306
Kafy Dier <works.omen@gmail.com>
2024-04-29 17:44:45 +0530

    profile page container animation done

--- js/page.js
+++ js/page.js
@@ -7,10 +7,26 @@ pages.forEach(page => {
                 if (otherPage !== page && otherPage.classList.contains('active')) {
                     otherPage.classList.remove('active');
                     otherPage.querySelector('.page-title').classList.remove('active');
+                    const containerProfile = otherPage.querySelector('.container-profile');
+                    if (containerProfile) {
+                        containerProfile.classList.remove('active');
+                    }
+                    const containerProfile2 = otherPage.querySelector('.container-profile-2');
+                    if (containerProfile2) {
+                        containerProfile2.classList.remove('active');
+                    }
                 }
             });
             page.classList.add('active');
             page.querySelector('.page-title').classList.add('active');
+            const containerProfile = page.querySelector('.container-profile');
+            if (containerProfile) {
+                containerProfile.classList.add('active');
+            }
+            const containerProfile2 = page.querySelector('.container-profile-2');
+            if (containerProfile2) {
+                containerProfile2.classList.add('active');
+            }
         }
     });
 });
